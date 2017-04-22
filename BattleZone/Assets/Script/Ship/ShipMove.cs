f479c2ccbbee725d384f420445cb2cd4c098b3a8
Ledgerofdeath <kwinfelt@gmail.com>
2017-04-22 19:18:15 +0200

    ShipMove
    SHIP CAN MOVE IT'S CRAZY !!!

---
+++ BattleZone/Assets/Script/Ship/ShipMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMove : MonoBehaviour {
+
+    public float _sensitive;
+    public Vector2 _orientationLimit = Vector2.zero;
+
+    public float Sensitive { get { return _sensitive; } }
+    public Vector2 OrentationLimit { get { return _orientationLimit; } }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update () {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if (vertical != 0)
+            MoveVertical(vertical);
+        if (horizontal != 0)
+            MoveHorizontal(horizontal);
+
+    }
+
+    public void MoveVertical(float speed)
+    {
+        transform.Translate(Vector3.forward * speed * _sensitive * Time.deltaTime);
+    }
+
+    public void MoveHorizontal(float speed)
+    {
+        transform.Translate(Vector3.right * speed * _sensitive * Time.deltaTime);
+    }
+}
