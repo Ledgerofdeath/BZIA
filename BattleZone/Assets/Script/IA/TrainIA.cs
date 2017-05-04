af60b7be169764be75a34e43afae25186f098158
Ultimawashi <clementfernandes2@gmail.com>
2017-05-04 17:46:37 +0200

    dk

--- BattleZone/Assets/Script/IA/TrainIA.cs
+++ BattleZone/Assets/Script/IA/TrainIA.cs
@@ -65,51 +65,6 @@ public class TrainIA : MonoBehaviour {
 		
 	}
 
-    public void InitPop()
-    {
-        int i = 0;
-        while (i < _taillePop)
-        {
-            
-            _neuralNetPop.Add(new NeuralNet(_uniformLaw,_config));
-            i++;
-        }
-    }
-
-    public void MutatePop(List<NeuralNet> bestNN)
-    {
-        foreach (NeuralNet n in bestNN)
-        {
-            float proba = UnityEngine.Random.Range(0, 1.0f);
-
-            if (proba < _probaMutation)
-            {
-                n.MutateBias(_normalLaw, _config);
-                n.MutateWeigh(_normalLaw, _config);
-            }
-        }
-    }
-
-    public void ReproducePop (List<NeuralNet> bestNN )
-    {
-        int i;
-        int j;
-        List<NeuralNet> temp = new List<NeuralNet>();
-
-        while (temp.Count < _taillePop)
-        {
-             i= UnityEngine.Random.Range(0, bestNN.Count);
-          
-            do
-            {
-                 j= UnityEngine.Random.Range(0, bestNN.Count);
-
-            } while (i == j);
-
-            temp.AddRange(bestNN[i].Reproduce2(bestNN[j], _config));
-        }
-
-        NeuralNetPop = temp;
-    }
+   
 
 }
