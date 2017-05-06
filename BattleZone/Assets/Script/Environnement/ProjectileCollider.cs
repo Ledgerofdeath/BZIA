74ada2227a8f7a7ebed31f5b36ddba15658a7e5c
Ledgerofdeath <kwinfelt@gmail.com>
2017-05-06 18:20:44 +0200

    projectil

---
+++ BattleZone/Assets/Script/Environnement/ProjectileCollider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileCollider : MonoBehaviour {
+    public GameObject _explosion = null;
+    void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(_explosion, transform.position, _explosion.transform.rotation);
+        Destroy(this.gameObject);
+    }
+}
---
+++ BattleZone/Assets/Script/Ship/ShipShoot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipShoot : MonoBehaviour {
+
+    public GameObject _projectile = null;
+    public GameObject _shootSpawn = null;
+    public GameObject _shipUp = null;
+
+    public float _delay = 0.0f;
+    float _currentDelay = 0.0f;
+
+    public float _speed = 0.0f;
+
+    public GameObject Projectile { set { _projectile = value; } }
+
+    private void Update()
+    {
+        if (Input.GetAxis("Fire1") != 0)
+        {
+            if (_currentDelay > _delay)
+            {
+                Shoot();
+                _currentDelay = 0.0f;
+            }
+        }
+        _currentDelay += Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        if (_projectile != null && _shootSpawn != null)
+        {
+            Quaternion r = transform.rotation;
+            GameObject p = Instantiate(_projectile, _shootSpawn.transform.position, _shootSpawn.transform.rotation) as GameObject;
+            p.GetComponent<Rigidbody>().AddForce(p.transform.forward * _speed, ForceMode.Impulse);
+        }
+    }
+}
